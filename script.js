fa727312cf772bd65a63584f81456ea1b975e9fd
Bibhuti <biswalbibhuti8@gmail.com>
2023-05-13 20:59:52 +0530

    Updated script.js
    Trial 1

--- script.js
+++ script.js
@@ -8,20 +8,49 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(function (student) {
+    if(student.marks>50){
+      console.log(student);
+    }
+  });
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(function (student) {
+    if(student.marks>50){
+      console.log(student);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newStudent={
+    id: 4,
+    name: "susan",
+    age: "20",
+    marks: 45
+  };
+  arr.push(newStudent);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  arr=arr.filter(function (student) {
+    return student.marks>=50;
+  });
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr2=[
+    { id: 5, name: "john_2", age: "180", marks: 800 },
+    { id: 6, name: "jack_2", age: "200", marks: 805 },
+    { id: 7, name: "karen_2", age: "190", marks: 305 }
+  ];
+  arr=arr.concat(arr2);
+  console.log(arr);
 }
